cf07f5db370217372cfe6963e2cbd587376c4327
Jake Vossen <jake@vossen.dev>
2019-11-16 08:07:04 -0700

    Added commas to setup.py, version bump

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setuptools
 setup(
-    name='sarcasm'
-    version='1.0.2'
-    description='Converts text to format for mocking spongebob meme'
-    author='Jake Vossen'
-    scripts=['bin/sarcasm']
-    author_email='jake@vossen.dev'
-    packages=[]
+    name='sarcasm',
+    version='1.0.3',
+    description='Converts text to format for mocking spongebob meme',
+    author='Jake Vossen',
+    scripts=['bin/sarcasm'],
+    author_email='jake@vossen.dev',
+    packages=[],
     install_requires=[]
 )
