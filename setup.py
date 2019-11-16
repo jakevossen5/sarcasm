469630ce8808ebf94ca3efdcf487090acb52f411
Jake Vossen <jake@vossen.dev>
2019-11-16 07:55:41 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setuptools
+ from setuptools import setuptools
 setup(
     name='sarcasm'
     version='1.0.0'
     description='Converts text to format for mocking spongebob meme'
     author='Jake Vossen'
-    scripts=[bin/sarcasm]
+    scripts=['bin/sarcasm']
     author_email='jake@vossen.dev'
     packages=[]
     install_requires=[]
